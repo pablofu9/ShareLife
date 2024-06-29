2c0e6cb40e82b36d6005699f8e7845c015011f1f
Pablo Fuertes <pfuertes@fiveflamesmobile.com>
2024-06-29 10:19:12 +0200

    Initial Commit

---
+++ ShareLife/ShareLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLifeApp.swift
+//  ShareLife
+//
+//  Created by Pablo Fuertes on 29/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ShareLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
